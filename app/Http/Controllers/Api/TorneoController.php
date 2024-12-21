bf83b7a027bcd92913f54ff53b685216be2ad2df
Juan Francisco Mannino Sanchez <juanfrancisco.m.sanchez@gmail.com>
2024-12-20 21:13:10 -0300

    Add documentacion
    Se agrega documentacion de swagger al proyecto

--- app/Http/Controllers/Api/TorneoController.php
+++ app/Http/Controllers/Api/TorneoController.php
@@ -13,7 +13,20 @@ use App\Models\Torneo;
 use App\Services\TorneoService;
 use Exception;
 use Illuminate\Http\Request;
+use OpenApi\Annotations as OA;
 
+
+/**
+ * @OA\Info(
+ *     title="Challenge Geopagos Documentacion",
+ *     version="1.0.0",
+ *     description="Challenge para Geopagos. Torneo de tenis",
+ *     @OA\Contact(
+ *          email="juanfrancisco.m.sanchez@gmail.com"
+ *      )
+ * )
+ * @OA\Server(url="http://localhost:8000")
+ */
 class TorneoController extends Controller
 {
 
@@ -22,7 +35,50 @@ class TorneoController extends Controller
     {
     }
     /**
-     * Display a listing of the resource.
+     * @OA\Get(
+     *     path="/api/torneos",
+     *     summary="Get torneos",
+     *     tags={"Torneos"},
+     *     description="Obtiene los torneos segun los filtros (fecha,tipo,nombreGanador)",
+     *     @OA\Parameter(
+     *         name="fecha",
+     *         in="query",
+     *         description="Fecha del torneo en formato d/m/Y",
+     *         required=false,
+     *         @OA\Schema(type="string", example="20/12/2024")
+     *     ),
+     *     @OA\Parameter(
+     *         name="tipo",
+     *         in="query",
+     *         description="Tipo de torneo (Masculino, Femenino)",
+     *         required=false,
+     *         @OA\Schema(type="string", example="Masculino")
+     *     ),
+     *     @OA\Parameter(
+     *         name="nombreGanador",
+     *         in="query",
+     *         description="Nombre del ganador del torneo",
+     *         required=false,
+     *         @OA\Schema(type="string", example="Jose")
+     *     ),
+     *     @OA\Response(
+     *          response=200,
+     *          description="Lista de torneos",
+     *          @OA\JsonContent(
+     *              @OA\Property(
+     *                  type="array",
+     *                  property="data",
+     *                  @OA\Items(
+     *                      @OA\Property(property="id", type="number", example="1"),
+     *                      @OA\Property(property="ganador", type="string", example="Jose"),
+     *                      @OA\Property(property="tipo_torneo", type="string", example="Masculino"),
+     *                      @OA\Property(property="cantidad_jugadores", type="string", example="8"),
+     *                      @OA\Property(property="fecha_torneo", type="string", example="20/12/2024")
+     *                  )
+     *              )
+     *          )
+     *      )
+     * )
      */
     public function index(GetTorneoRequest $request)
     {
@@ -53,7 +109,43 @@ class TorneoController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * @OA\Get(
+     *     path="/api/torneos/{id}",
+     *     summary="Detalles de un torneo",
+     *     description="Devuelve los detalles de un torneo pasado como parametro.",
+     *     tags={"Torneos"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="Id del torneo",
+     *         required=true,
+     *         @OA\Schema(type="integer", example=5)
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Detalles del torneo",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="data",
+     *                 type="object",
+     *                 @OA\Property(property="id", type="integer", example=5),
+     *                 @OA\Property(property="ganador", type="string", example="Jose"),
+     *                 @OA\Property(property="tipo_torneo", type="string", example="Masculino"),
+     *                 @OA\Property(property="cantidad_jugadores", type="string", example="8"),
+     *                 @OA\Property(property="fecha_torneo", type="string", example="20/12/2024", description="Fecha del torneo en formato d/m/Y")
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Error: Not Found",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="error", type="string", example="El recurso solicitado no existe")
+     *         )
+     *     )
+     * )
      */
     public function show(Torneo $torneo)
     {
@@ -78,6 +170,65 @@ class TorneoController extends Controller
         //
     }
 
+
+    /**
+     * @OA\Get(
+     *     path="/api/torneoFemenino",
+     *     summary="Inicia Torneo Femenino",
+     *     tags={"Torneo"},
+     *     description="Recibe una lista de jugadoras con sus atributos(nombre,habilidad,reaccion) y devuelve la ganadora del torneo.",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         description="Lista de jugadoras con sus atributos, potencias de dos",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(
+     *                 type="object",
+     *                 required={"nombre", "habilidad", "fuerza", "velocidad"},
+     *                 @OA\Property(property="nombre", type="string", example="Lucia"),
+     *                 @OA\Property(property="habilidad", type="integer", example=90),
+     *                 @OA\Property(property="reaccion", type="integer", example=100),
+     *             ),
+     *             example={
+     *                 {
+     *                     "nombre": "Lucia",
+     *                     "habilidad": 90,
+     *                     "reaccion": 100
+     *                 },
+     *                 {
+     *                     "nombre": "Maria",
+     *                     "habilidad": 80,
+     *                     "reaccion": 85
+     *                 }
+     *              }
+     *         )
+     *     ),
+     *      @OA\Response(
+     *         response=200,
+     *         description="Torneo finalizado con éxito",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="mensaje", type="string", example="Torneo finalizado"),
+     *             @OA\Property(
+     *                 property="ganadora",
+     *                 type="object",
+     *                 @OA\Property(property="nombre", type="string", example="Lucia"),
+     *                 @OA\Property(property="habilidad", type="integer", example=90),
+     *                 @OA\Property(property="reaccion", type="integer", example=100),
+     *             )
+     *         )
+     *     ),
+     *      @OA\Response(
+     *         response=400,
+     *         description="Error en la solicitud",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="error", type="string", example="Datos inválidos")
+     *         )
+     *     )
+     * )
+     *
+     */
     public function torneoFemenino(TorneoFemeninoRequest $request){
         try {
             $jugadores = collect($request->validated())->map(function ($data) {
@@ -103,6 +254,68 @@ class TorneoController extends Controller
     }
 
 
+    /**
+     * @OA\Get(
+     *     path="/api/torneoMasculino",
+     *     summary="Inicia Torneo Masculino",
+     *     tags={"Torneo"},
+     *     description="Recibe una lista de jugadores con sus atributos(nombre,habilidad,fuerza,velocidad) y devuelve el ganador del torneo.",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         description="Lista de jugadores con sus atributos, potencias de dos",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(
+     *                 type="object",
+     *                 required={"nombre", "habilidad", "fuerza", "velocidad"},
+     *                 @OA\Property(property="nombre", type="string", example="Pedro"),
+     *                 @OA\Property(property="habilidad", type="integer", example=90),
+     *                 @OA\Property(property="fuerza", type="integer", example=100),
+     *                 @OA\Property(property="velocidad", type="integer", example=30)
+     *             ),
+     *               example={
+     *                 {
+     *                     "nombre": "Pedro",
+     *                     "habilidad": 90,
+     *                     "fuerza": 100,
+     *                     "velocidad": 30
+     *                 },
+     *                 {
+     *                     "nombre": "Jose",
+     *                     "habilidad": 90,
+     *                     "fuerza": 60,
+     *                     "velocidad": 85
+     *                 }
+     *             }
+     *         )
+     *     ),
+     *      @OA\Response(
+     *         response=200,
+     *         description="Torneo finalizado con éxito",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="mensaje", type="string", example="Torneo finalizado"),
+     *             @OA\Property(
+     *                 property="ganador",
+     *                 type="object",
+     *                 @OA\Property(property="nombre", type="string", example="Jose"),
+     *                 @OA\Property(property="habilidad", type="integer", example=90),
+     *                 @OA\Property(property="velocidad", type="integer", example=60),
+     *                 @OA\Property(property="fuerza", type="integer", example=85),
+     *             )
+     *         )
+     *     ),
+     *      @OA\Response(
+     *         response=500,
+     *         description="Error en la solicitud",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="error", type="string", example="Server error")
+     *         )
+     *     )
+     * )
+     *
+     */
     public function torneoMasculino (TorneoMasculinoRequest $request){
         try {
             $jugadores = collect($request->validated())->map(function ($data) {
--- app/Models/Torneo.php
+++ app/Models/Torneo.php
@@ -4,7 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Schema(
+ *     schema="torneo",
+ *     type="object",
+ *     title="Torneo",
+ *     @OA\Property(property="id",type="integer",example=1),
+ *     @OA\Property(property="nombreGanador",type="string",example="Jose"),
+ *     @OA\Property(property="tipoTorneo",type="string",example="Masculino"),
+ *     @OA\Property(property="cantidadJugadores",type="string",example="8"),
+ *     @OA\Property(property="created_at", type="string", format="date-time", example="2024-12-19 23:29:07"),
+ *     @OA\Property(property="updated_at", type="string", format="date-time", example="2024-12-19 23:29:07")
+ * )
+ */
 class Torneo extends Model
 {
     use HasFactory;
